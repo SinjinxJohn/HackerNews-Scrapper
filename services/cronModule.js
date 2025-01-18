85c56ffdf1814756b29aae66640dd60910035db6
Sinjin John <sinjin.hotlinebling@gmail.com>
2025-01-18 14:29:16 +0530

    changed cron job freq

--- services/cronModule.js
+++ services/cronModule.js
@@ -4,7 +4,7 @@ const {broadcast} = require('./websocket');
 
 
 const scheduleScraping = (wss)=>{
-    const job = new cron.CronJob('*/30 * * * * *', async () => {
+    const job = new cron.CronJob('*/5 * * * * *', async () => {
         console.log('Scraping Hacker News...');
         const stories = await scrapeNews();
         broadcast(wss, { newStories: stories });
